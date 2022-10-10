e3774efdb14cce2a225fc4a35dea0151c9f65dc6
abooBala <aboobala64@gmail.com>
2022-10-11 00:54:04 +0100

    Created first post request

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
+@app.get("/")
+def home():
+    return{"data": "Welcome to homepage"}
+
+
 @app.get("/welcome")
 def root():
-    return {"message": "Welcome to my API"}
+    return {"message": "This is the welcome page"}
 
 
 @app.get("/posts")
 def get_posts():
     return {"data": "This is a random post"}
+
+# Post request 
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}

74c68fae841d811a3189a0292e78f01825c0fb78
abooBala <aboobala64@gmail.com>
2022-10-14 02:51:06 +0100

    Basemodel from pydantic

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
@@ -18,8 +23,16 @@ def root():
 def get_posts():
     return {"data": "This is a random post"}
 
-# Post request 
-@app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+# Post request (Bad way to create Posts) 
+# @app.post("/createpost")
+# def create_post(payload: dict = Body(...)):
+#    print(payload)
+#    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+
+# Validating data with Schema 
+
+@app.post("/createposts")
+def create_post(new_post: Post):
+    print(new_post.title)
+    print(new_post.content)
+    return{"data": "New post"}
